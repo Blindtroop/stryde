ea6c7110d6baabd04edbf3a844beff465d78409e
Daniel Kaluhi <kaluhilance4@gmail.com>
2026-06-29 08:11:44 +0300

    reverted back to 404

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Action from "./assets/cta/Action";
 import TrustedPartners from "./assets/partners/partners";
 import Shopping from "./assets/shopping/shopping";
 import CartModal from "./assets/cart/cart";
+import ComingSoon from "./assets/404/404"
 import Footer from "./assets/footer/footer";
 
 import "./App.css";
@@ -17,13 +18,14 @@ import "./App.css";
 function Home({ onAddToBag, onOpenCart }) {
   return (
     <>
-      <Hero />
+      <ComingSoon />
+      {/* <Hero />
       <Arrivals onAddToBag={onAddToBag} />
       <Flash onAddToBag={onAddToBag} onOpenCart={onOpenCart} />
       <Categories />
       <Action />
       <TrustedPartners />
-      <Footer />
+      <Footer /> */}
     </>
   );
 }
@@ -86,7 +88,7 @@ export default function App() {
 
   return (
     <>
-      <Navbar bagCount={bagCount} onOpenCart={() => setCartOpen(true)} />
+      {/* <Navbar bagCount={bagCount} onOpenCart={() => setCartOpen(true)} /> */}
 
       <Routes>
         <Route
